b090d86fc25dbd639cfb6eafd0f73ba49f996040
Priyanshu Kumar <pk953666@gmail.com>
2026-05-26 08:31:37 +0530

    constants for application:

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -3,6 +3,3 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final isLoadingProvider = StateProvider<bool>((ref) => false);
 final errormessageProvider = StateProvider<String?>((ref) => null);
 
-final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
-  return AuthNotifier();
-});
