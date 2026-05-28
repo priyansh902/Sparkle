cefef49c7d40d3fbac80b63a21b60cedeff4d005
Priyanshu Kumar <pk953666@gmail.com>
2026-05-28 21:06:15 +0530

    post fail actions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,17 @@
+buildscript {
+    ext.kotlin_version = '2.0.21'  
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath 'com.android.tools.build:gradle:7.3.0'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath 'com.google.gms:google-services:4.4.2'
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -21,14 +35,4 @@ subprojects {
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.4")
-    }
 }
