639554ed25dcb6c0d1269924253898089d167f9f
Priyanshu Kumar <pk953666@gmail.com>
2026-06-02 20:11:44 +0530

    main merge

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyBjB7C6SCwBtd0AtagVy4NLCbjqa4se0Y4",
+      appId: "1:655837677296:web:7b8130c0525d21b466e435",
+      messagingSenderId: "655837677296",
+      projectId: "sparkle-lite-78f1c",
+      authDomain: "sparkle-lite-78f1c.firebaseapp.com",
+      storageBucket: "sparkle-lite-78f1c.firebasestorage.app",
+    );
+  }
+}
